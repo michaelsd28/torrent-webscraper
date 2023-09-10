3903400c0b108ee49ca7492a43b64c6a1a799939
Michael <rd28@hotmail.es>
2023-09-10 18:44:52 -0400

    suggestions added

--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 import { NgbTypeahead } from '@ng-bootstrap/ng-bootstrap';
 import {
   OperatorFunction,
@@ -17,105 +18,93 @@ import {
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],
 })
-export class NavbarComponent {
-  states = [
-    'Alabama',
-    'Alaska',
-    'American Samoa',
-    'Arizona',
-    'Arkansas',
-    'California',
-    'Colorado',
-    'Connecticut',
-    'Delaware',
-    'District Of Columbia',
-    'Federated States Of Micronesia',
-    'Florida',
-    'Georgia',
-    'Guam',
-    'Hawaii',
-    'Idaho',
-    'Illinois',
-    'Indiana',
-    'Iowa',
-    'Kansas',
-    'Kentucky',
-    'Louisiana',
-    'Maine',
-    'Marshall Islands',
-    'Maryland',
-    'Massachusetts',
-    'Michigan',
-    'Minnesota',
-    'Mississippi',
-    'Missouri',
-    'Montana',
-    'Nebraska',
-    'Nevada',
-    'New Hampshire',
-    'New Jersey',
-    'New Mexico',
-    'New York',
-    'North Carolina',
-    'North Dakota',
-    'Northern Mariana Islands',
-    'Ohio',
-    'Oklahoma',
-    'Oregon',
-    'Palau',
-    'Pennsylvania',
-    'Puerto Rico',
-    'Rhode Island',
-    'South Carolina',
-    'South Dakota',
-    'Tennessee',
-    'Texas',
-    'Utah',
-    'Vermont',
-    'Virgin Islands',
-    'Virginia',
-    'Washington',
-    'West Virginia',
-    'Wisconsin',
-    'Wyoming',
-  ];
+export class NavbarComponent implements OnInit {
+
+
+  constructor(private router:Router) {}
+
+  ngOnInit(): void {
+    let localStorageSuggestions = localStorage.getItem('_suggestions');
+    if (localStorageSuggestions) {
+      let suggestionsArray = JSON.parse(localStorageSuggestions);
+      this.suggestions = suggestionsArray;
+    } else if (!localStorage.getItem('_suggestions')) {
+      localStorage.setItem('_suggestions', JSON.stringify(this.suggestions) );
+    }
+
+
+  }
+  suggestions: string[] = [];
   inputTerm: string = '';
 
-  @ViewChild('instance', { static: true }) instance: NgbTypeahead = {} as NgbTypeahead;
-	focus$ = new Subject<string>();
-	click$ = new Subject<string>();
+  @ViewChild('instance', { static: true }) instance: NgbTypeahead =
+    {} as NgbTypeahead;
+  focus$ = new Subject<string>();
+  click$ = new Subject<string>();
 
-  searchTypeHead:  OperatorFunction<string, readonly string[]> = (text$: Observable<string>) => {
-		const debouncedText$ = text$.pipe(debounceTime(200), distinctUntilChanged());
-		const clicksWithClosedPopup$ = this.click$.pipe(filter(() => !this.instance.isPopupOpen()));
-		const inputFocus$ = this.focus$;
+  searchTypeHead: OperatorFunction<string, readonly string[]> = (
+    text$: Observable<string>
+  ) => {
+    const debouncedText$ = text$.pipe(
+      debounceTime(200),
+      distinctUntilChanged()
+    );
+    const clicksWithClosedPopup$ = this.click$.pipe(
+      filter(() => !this.instance.isPopupOpen())
+    );
+    const inputFocus$ = this.focus$;
 
-		return merge(debouncedText$, inputFocus$, clicksWithClosedPopup$).pipe(
-			map((term) =>
-				(term === '' ? this.states : this.states.filter((v) => v.toLowerCase().indexOf(term.toLowerCase()) > -1)).slice(0, 5),
-			),
-		);
-	};
+    return merge(debouncedText$, inputFocus$, clicksWithClosedPopup$).pipe(
+      map((term) =>
+        (term === ''
+          ? this.suggestions
+          : this.suggestions.filter(
+              (v) => v.toLowerCase().indexOf(term.toLowerCase()) > -1
+            )
+        ).slice(0, 5)
+      )
+    );
+  };
 
-  deleteState(state: string) {
-    let index = this.states.indexOf(state);
+ // Remove the state from the list of suggestions
+ deleteState(state: string) {
+    let index = this.suggestions.indexOf(state);
     if (index > -1) {
-      this.states = this.states.filter((v) => v !== state);
+      this.suggestions = this.suggestions.filter((v) => v !== state);
     }
     // prevent closing dropdown when clicked
     event!.stopPropagation();
 
-let tempSearchTerm = this.inputTerm;
-   // close dropdown
-   this.instance.dismissPopup();
-
-    // open dropdown
+    // If the user has already typed something, reopen the dropdown
+    // with the user's current input as the search term
+    let tempSearchTerm = this.inputTerm;
+    this.instance.dismissPopup();
     this.click$.next(tempSearchTerm);
+  }
 
 
+  submitSearchTerm() {
 
+    this.inputTerm = this.inputTerm.trim();
+
+    if (this.inputTerm.length > 0) {
+      if (this.suggestions.indexOf(this.inputTerm) === -1) {
+        this.suggestions = [this.inputTerm, ...this.suggestions];
+        localStorage.setItem('_suggestions', JSON.stringify(this.suggestions));
+      }
+    }
+
+    this.router.navigate(['/torrents/anime/'+this.inputTerm]);
 
 
 
   }
+
+  openDropDown() {
+   if (this.suggestions.length > 0) {
+    this.click$.next(this.inputTerm);
+   }
+  }
 }
+
+
